60f5e925293f01b9ecaf7f653ec0d3907a4a225f
glebkon <43175255+glebkon@users.noreply.github.com>
2022-01-18 22:52:41 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(284, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Построить систему координат/очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(468, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(518, 508);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(1, 363);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(247, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(24, 346);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Масштаб: 1Х";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(316, 276);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 155);
+            this.trackBar2.TabIndex = 6;
+            this.trackBar2.Value = 5;
+            this.trackBar2.Scroll += new System.EventHandler(this.TrackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(393, 276);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(45, 155);
+            this.trackBar3.TabIndex = 7;
+            this.trackBar3.Value = 5;
+            this.trackBar3.Scroll += new System.EventHandler(this.TrackBar3_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(259, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Разтягивание и расширение";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(250, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 22);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(250, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(211, 22);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(207, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "По X:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(207, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "По Y:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(304, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Смещение графика";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(262, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 46);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Сместить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(295, 480);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(167, 46);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Загрузить файл";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(468, 544);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(518, 22);
+            this.textBox3.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(313, 544);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 16);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Загруженный файл:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(16, 133);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(117, 20);
+            this.checkBox1.TabIndex = 25;
+            this.checkBox1.Text = "Отображение";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(16, 188);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(117, 20);
+            this.checkBox2.TabIndex = 26;
+            this.checkBox2.Text = "Отображение";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.CheckBox2_CheckedChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(47, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 16);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "встроенной функции";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(52, 211);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(131, 16);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "функции из файла";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(998, 586);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "Form1";
+            this.Text = "Func building";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        double h, x, y, n, TrackBarScal = 5, WidthScal = 5, HeightScal = 5, moveScale,moveScale2;
+        double x1 = -5, x2 = 5, y1 = -5, y2 = 5;
+        int i1, i2, j1, j2;
+        string line;
+        string a;
+        bool draw_func = false;
+        bool draw_graf_file = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            CheckBox checkBox = (CheckBox)sender; // приводим отправителя к элементу типа CheckBox
+            if (checkBox.Checked == true)
+            {
+                draw_func = true;
+            }
+            else
+            {
+                draw_func = false;
+            }
+            draw_graf();
+            draw_graf_listbox();
+
+        }
+        private void CheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (textBox3.TextLength == 0)
+            {
+                if(checkBox2.Checked==true) MessageBox.Show("Файл не загружен. Отображение невозможно!");
+                checkBox2.Checked = false;
+            }
+            else
+            {
+                CheckBox checkBox = (CheckBox)sender; // приводим отправителя к элементу типа CheckBox
+                if (checkBox.Checked == true)
+                {
+                    draw_graf_file = true;
+                }
+                else
+                {
+                    draw_graf_file = false;
+                }
+                draw_graf();
+                draw_graf_listbox();
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            draw_func = false;
+            draw_graf_file = false;
+            trackBar1.Value = 5;
+            trackBar2.Value = 5;
+            trackBar3.Value = 5;
+            TrackBarScal = 5; WidthScal = 5; HeightScal = 5;
+            label1.Text = "Масштаб: " + 1 + "X";
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            moveScale = 0;
+            moveScale2 = 0;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            draw_graf();
+            draw_graf_listbox();
+        }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength!=0) moveScale = double.Parse(textBox1.Text) * 50;
+            else moveScale = 0;
+            if (textBox2.TextLength != 0) moveScale2 = double.Parse(textBox2.Text) * (-50);
+            else moveScale2 = 0;
+
+            draw_graf();
+            draw_graf_listbox();
+        }
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Файлы текста (*.txt) | *.txt";
+            openFileDialog1.ShowDialog();
+            a = openFileDialog1.FileName;
+            textBox3.Text = a;
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            TrackBarScal = trackBar1.Value;
+            draw_graf();
+            draw_graf_listbox();
+
+            label1.Text = "Масштаб: " + TrackBarScal / 5 + "X";
+        }
+        private void TrackBar2_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar2.Value != 0) HeightScal = trackBar2.Value;
+            else trackBar2.Value = 1;
+            draw_graf();
+            draw_graf_listbox();
+        }
+        private void TrackBar3_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar3.Value != 0) WidthScal = trackBar3.Value;
+            else trackBar3.Value = 1;
+            draw_graf();
+            draw_graf_listbox();
+        }
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        double function_for_show(double xt)
+        {
+
+            double ff;
+
+            if ((xt < 10) && (xt > 5))
+            {
+                ff = (Math.Abs(xt * Math.Log(xt - 4)) * Math.Sqrt(xt)) * (1 / Math.Pow(Math.E, (4 * xt - 1) / 5));
+            }
+            else if ((xt <= 1) && (xt >= 0.1)||(xt <= -0.1) && (xt > -3))
+            {
+                ff = Math.Pow(xt, -5); // при -20 переполнение стэка (это нормально)
+            }
+            else if ((xt < 0.1) && (xt > -0.1)) // для обхода точки, где функция = +00
+            {
+                ff = -100;
+            }
+            else
+            {
+                ff = Math.Pow(xt, -10);
+            }
+            return ff;
+        }
+        void system_koordinat(double x1, double x2, double y1, double y2)
+        {
+            Pen pen_setka = new Pen(Brushes.DarkGray, 1);
+            pen_setka.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            for(int p = (int)x1;p<=(int)x2;p++)
+            {
+                pictureBox1.CreateGraphics().DrawLine(pen_setka, xtoi(p), ytoj(y2), xtoi(p), ytoj(y1));
+            }
+            for (int p = (int)y1; p <= (int)y2; p++)
+            {
+                pictureBox1.CreateGraphics().DrawLine(pen_setka, xtoi(x1), ytoj(p), xtoi(x2), ytoj(p));
+            }
+            Pen pen_os = new Pen(Brushes.White, 1);
+            pen_os.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            pen_os.StartCap = System.Drawing.Drawing2D.LineCap.Triangle;
+
+            pictureBox1.CreateGraphics().DrawLine(pen_os, xtoi(x1), ytoj(0), xtoi(x2), ytoj(0));
+            pictureBox1.CreateGraphics().DrawLine(pen_os, xtoi(0), ytoj(y1), xtoi(0), ytoj(y2));
+
+            Font MyFont = new Font("arial", 8, FontStyle.Regular);
+
+            for (double p = x1; p <= x2; p++)
+                pictureBox1.CreateGraphics().DrawString(Convert.ToString(p), MyFont, Brushes.White, new Point(xtoi(p - 0.2), ytoj(-0.05)));
+            for (int p = 1; p <= y2; p++)
+                pictureBox1.CreateGraphics().DrawString(Convert.ToString(p), MyFont, Brushes.White, new Point(xtoi(-0.5), ytoj(p + 0.1)));
+            for (int p = -1; p >= y1; p--)
+                pictureBox1.CreateGraphics().DrawString(Convert.ToString(p), MyFont, Brushes.White, new Point(xtoi(-0.6), ytoj(p + 0.1)));
+
+        }
+        void draw_graf_listbox()
+        {
+
+
+            if (draw_graf_file)
+            {
+                StreamReader sr = new StreamReader(a);
+
+                int i = 0;
+                double[] Arr = new double[1000];
+                while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+                    Arr[i] = double.Parse(line);
+                    i++;
+                }
+                sr.Close();
+
+
+
+                for (int j = 0; j < i - 3; j += 2)
+                {
+
+                    float q, w, e, r;
+                    q = (float)(xtoi(Arr[j]* HeightScal / 5) + moveScale / (TrackBarScal / 5));
+                    w = (float)(ytoj(Arr[j + 1] * (WidthScal / 5)) + moveScale2 / (TrackBarScal / 5));
+                    e = (float)(xtoi(Arr[j + 2]* HeightScal / 5) + moveScale / (TrackBarScal / 5));
+                    r = (float)(ytoj(Arr[j + 3] * (WidthScal / 5)) + moveScale2 / (TrackBarScal / 5));
+                    pictureBox1.CreateGraphics().DrawLine(Pens.Red, q, w, e, r);
+                }
+            }
+
+
+
+        }
+        void draw_graf()
+        {
+            x1 = -5 * (TrackBarScal / 5) ;
+            x2 = 5 * (TrackBarScal / 5) ;
+            y1 = -5 * (TrackBarScal / 5) ;
+            y2 = 5 * (TrackBarScal / 5) ;
+            pictureBox1.CreateGraphics().Clear(Color.Gray);
+
+            i1 = 0;
+            j1 = 0;
+            i2 = pictureBox1.Width - 1;
+            j2 = pictureBox1.Height - 1;
+            n = pictureBox1.Width;
+            system_koordinat(x1, x2, y1, y2);
+            h = (x2 - x1) / n;
+            x = x1;
+           //y = function_for_show(x);
+            if (draw_func)
+            {
+                while (x < x2)
+                {
+                    float q, w, e, r;
+                    q = (float)(xtoi(x * HeightScal / 5) + moveScale / (TrackBarScal / 5));
+                    w = (float)(ytoj(function_for_show(x)* (WidthScal / 5)) + moveScale2 / (TrackBarScal / 5));
+                    e = (float)(xtoi((x + h) * (HeightScal / 5)) + moveScale / (TrackBarScal / 5));
+                    r = (float)(ytoj(function_for_show(x + h) * (WidthScal / 5)) + moveScale2 / (TrackBarScal / 5));
+                    pictureBox1.CreateGraphics().DrawLine(Pens.Blue, q, w, e, r);
+                    x = x + h;
+                }
+            }
+        }
+        int xtoi(double x)
+        {
+            int ii;
+            ii = i1 + (int)Math.Truncate((x - x1) * ((i2 - i1) / (x2 - x1)));
+            return ii;
+        }
+        int ytoj(double y)
+        {
+            int jj;
+            jj = j2 + (int)Math.Truncate((y - y1) * (j1 - j2) / (y2 - y1));
+            return jj;
+        }
+    }
+}
